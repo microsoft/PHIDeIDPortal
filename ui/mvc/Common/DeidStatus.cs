2319a5a81a4e98cb8f7467e118af84e7a840ac21
Andy Badera <andybadera@microsoft.com>
2024-04-21 10:58:12 -0500

    Adding denied.

---
+++ ui/mvc/Common/DeidStatus.cs
@@ -0,0 +1,11 @@
+﻿namespace PhiDeidPortal.Ui.Common
+{
+    public enum DeidStatus
+    {
+        Uploaded = 1,
+        RequiresJustification = 2,
+        JustificationApprovalPending = 3,
+        Completed = 4,
+        Denied = 5
+    }
+}
