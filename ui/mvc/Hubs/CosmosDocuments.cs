514005e339e52531c37c9ac0797ae5d935fd2407
Mr. Cage <lcage@go-planet.com>
2024-12-09 17:28:10 -0500

    add signalr basics

---
+++ ui/mvc/Hubs/CosmosDocuments.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace PhiDeidPortal.Ui.Hubs
+{
+    public class CosmosDocuments : Hub
+    {
+        public async Task SendMessage(CosmosDbDocument doc)
+        {
+            await Clients.All.SendAsync("ReceiveDocument", doc.User, doc.Message);            
+        }
+        public async Task UpdateCounts(CosmosDbDocument doc)
+        {            
+            await Clients.All.SendAsync("UpdateCounts", new StatusSummary());
+        }
+    }
+
+    public class CosmosDbDocument
+    {
+        public string User { get; set; }
+        public string Message { get; set; }
+    }
+
+    public class StatusSummary
+    {
+        public int TotalCount { get; set; } = 1;
+        public int UnprocessedCount { get; set; } = 2;
+        public int JustificationCount { get; set; } = 3;
+        public int ReviewCount { get; set; } = 4;
+    }
+}
--- ui/mvc/Program.cs
+++ ui/mvc/Program.cs
@@ -20,6 +20,8 @@ using Microsoft.Identity.Web.UI;
 using Microsoft.FeatureManagement;
 using PhiDeidPortal.Ui.Services;
 using System.Net.Http;
+using System.Text.Json;
+using PhiDeidPortal.Ui.Hubs;
 
 namespace PhiDeidPortal.Ui
 {
@@ -28,6 +30,13 @@ namespace PhiDeidPortal.Ui
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddSignalR()
+                    .AddJsonProtocol(options =>
+                    {
+                        options.PayloadSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+                        options.PayloadSerializerOptions.WriteIndented = true;
+                    });
 
             builder.Services.AddFeatureManagement();
 
@@ -137,11 +146,15 @@ namespace PhiDeidPortal.Ui
 
             app.UseStaticFiles();
 
-            app.UseRouting();
+            app.UseRouting();            
 
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+            app.UseEndpoints(endpoints => 
+            {
+                endpoints.MapHub<CosmosDocuments>("/cosmosdocuments");
+                endpoints.MapControllers(); 
+            });
 
             app.MapRazorPages();
 
